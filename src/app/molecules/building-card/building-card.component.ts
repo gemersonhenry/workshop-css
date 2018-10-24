0ada80a02955a3664a1c3de1e804f4cf3ad8c60e
BZGemerson <gbendezu@everis.com>
2018-10-24 18:01:55 -0500

    feat(workshop-css): configuración inicial de media querys

--- src/app/molecules/building-card/building-card.component.ts
+++ src/app/molecules/building-card/building-card.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-building-card',
   templateUrl: './building-card.component.html',
-  styleUrls: ['./building-card.component.scss']
+  styleUrls: [
+    './building-card.component.scss'
+  ]
 })
 export class BuildingCardComponent implements OnInit {
 
